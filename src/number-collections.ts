dfb3f67fe63c1a279432a2587152098b39a7e2c9
Jeff Wong <v-jwong1@expedia.com>
2020-05-23 16:46:16 +1000

    create NumberCollections class to wrap collection object

---
+++ src/number-collections.ts
@@ -0,0 +1,21 @@
+export default class NumberCollections {
+  private collection: { [key: string]: number };
+
+  constructor() {
+    this.collection = {};
+  }
+
+  set(key: number): void {
+    const currentValue = this.collection[key];
+
+    this.collection[key] = currentValue ? currentValue + 1 : 1;
+  }
+
+  toString(): string {
+    return Object                         // { 1: 2, 2: 8, 3: 5, 4: 6 }
+      .entries(this.collection)           // [ [1, 2], [2, 8], [3, 5], [4, 6] ]
+      .sort((a, b) => b[1] - a[1])        // [ [2, 8], [4, 6], [3, 5], [1, 2] ]
+      .map(arr => `${arr[0]}:${arr[1]}`)  // [ "2:8", "4:6", "3:5", "1:2" ]
+      .join(", ");
+  }
+}
