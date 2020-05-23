912f04bfa6af13b856efae085d3d82e8f4378b59
Jeff Wong <v-jwong1@expedia.com>
2020-05-23 16:45:59 +1000

    create Main class for main program to run

---
+++ src/main.ts
@@ -0,0 +1,158 @@
+import readline, { Interface } from "readline";
+import NumberCollections from "./number-collections";
+import { Keywords, ConsoleMessages } from "./enums";
+
+export default class Main {
+  private reader: Interface;
+  private intervalId: NodeJS.Timeout;
+  private emitTimeInMilliseconds: number;
+  private numberCollections: NumberCollections;
+  private startTime: number;
+  private remainingTime: number;
+  private fibonacciSeries: bigint[] = [];
+
+  constructor(numberCollections: NumberCollections) {
+    this.numberCollections = numberCollections;
+
+    this.reader = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    this.reader.on("close", function() {
+      process.exit(0);
+    });
+  }
+
+  generateFibonacciSeries(): void {
+    this.fibonacciSeries = [0n, 1n];
+
+    for (let i = 2; i <= 1000; i++) {
+      this.fibonacciSeries = [...this.fibonacciSeries, this.fibonacciSeries[i - 2] + this.fibonacciSeries[i - 1]];
+
+      console.log(this.fibonacciSeries[i]);
+    }
+  }
+
+  quit(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+    console.log(this.numberCollections.toString());
+    console.log(ConsoleMessages.EXIT_MESSAGE);
+    this.reader.close();
+  }
+
+  checkQuit(input: string): void {
+    if (input === Keywords.QUIT) {
+      this.quit();
+    }
+  }
+
+  isInvalidNumber(input: number): boolean {
+    return isNaN(input);
+  }
+
+  checkFibonacci(input: string): void {
+    if (input.includes(".") || Number(input) < 0) { // Do not consider negative numbers and numbers with decimal points
+      return;
+    }
+
+    const isFibonacci = this.fibonacciSeries.includes(BigInt(input));
+
+    if (isFibonacci) {
+      console.log(ConsoleMessages.FIBONACCI_MESSAGE);
+    }
+  }
+
+  setConsoleEmitInterval(): void {
+    this.intervalId = setInterval(() => {
+      this.startTime = new Date().getTime();
+      console.log(this.numberCollections.toString());
+      console.log(ConsoleMessages.ENTER_NEXT_NUMBER_NO_NEWLINE);
+    }, this.emitTimeInMilliseconds);
+  }
+
+  handleTimerResumed(): void {
+    console.log(ConsoleMessages.TIMER_RESUMED);
+
+    setTimeout(() => {
+      console.log(this.numberCollections.toString());
+      this.setConsoleEmitInterval();
+      this.getNextNumber();
+    }, this.remainingTime);
+  }
+
+  handleTimerHalted(): void {
+    this.reader.question(ConsoleMessages.TIMER_HALTED, (input: string) => {
+      this.checkQuit(input);
+
+      if (input !== Keywords.RESUME) {
+        console.log(ConsoleMessages.ENTER_RESUME_OR_QUIT);
+        return this.handleTimerHalted();
+      }
+
+      this.handleTimerResumed();
+    });
+  }
+
+  getNextNumber(): void {
+    this.reader.question(ConsoleMessages.ENTER_NEXT_NUMBER, (input: string) => {
+      this.checkQuit(input);
+
+      if (input === Keywords.HALT) {
+        clearInterval(this.intervalId);
+        this.remainingTime = this.emitTimeInMilliseconds - (new Date().getTime() - this.startTime);
+        return this.handleTimerHalted();
+      }
+
+      const inputNumber = parseFloat(input);
+
+      if (this.isInvalidNumber(inputNumber)) {
+        console.log(ConsoleMessages.INVALID_NEXT_NUMBER_INPUT);
+        return this.getNextNumber();
+      }
+
+      this.numberCollections.set(inputNumber);
+      this.checkFibonacci(input);
+      this.getNextNumber();
+    });
+  }
+
+  getFirstNumber(): void {
+    this.reader.question(ConsoleMessages.ENTER_FIRST_NUMBER, (input: string) => {
+      this.checkQuit(input);
+
+      const inputNumber = parseFloat(input);
+
+      if (this.isInvalidNumber(inputNumber)) {
+        console.log(ConsoleMessages.INVALID_FIRST_NUMBER_INPUT);
+        return this.getFirstNumber();
+      }
+
+      this.numberCollections.set(inputNumber);
+      this.checkFibonacci(input);
+      this.setConsoleEmitInterval();
+      this.getNextNumber();
+    });
+  }
+
+  run(): void {
+    this.generateFibonacciSeries();
+
+    this.reader.question(ConsoleMessages.ENTER_EMIT_SECONDS, (input: string) => {
+      this.checkQuit(input);
+
+      const inputNumber = parseFloat(input);
+
+      if (inputNumber <= 0 || this.isInvalidNumber(inputNumber)) {
+        console.log(ConsoleMessages.INVALID_EMIT_SECONDS_INPUT);
+        return this.run();
+      }
+
+      this.checkFibonacci(input);
+      this.emitTimeInMilliseconds = inputNumber * 1000;
+      this.getFirstNumber();
+    });
+  }
+}
